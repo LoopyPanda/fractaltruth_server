d30d0e6e59c445b4d5a6cd719476dece4089d2ae
LoopyPanda <abhilashakatkiya@gmail.com>
2022-12-07 10:49:44 +0100

    add contact page code

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const cors = require("cors");
 const nodemailer = require("nodemailer");
+//const { google } = require("googleapis");
+//const OAuth2 = google.auth.OAuth2;
 const app = express();
+
+app.use(cors());
 require("dotenv").config();
+/*
+const oauth2Client = new OAuth2(
+    process.env.OAUTH_CLIENTID, // ClientID
+    process.env.OAUTH_CLIENT_SECRET, // Client Secret
+    "https://developers.google.com/oauthplayground" // Redirect URL
+);
+
+oauth2Client.setCredentials({
+    refresh_token: process.env.OAUTH_REFRESH_TOKEN,
+});
+const accessToken = oauth2Client.getAccessToken()
+*/
 
 // following this link --- https://dev.to/jlong4223/how-to-implement-email-functionality-with-node-js-react-js-nodemailer-and-oauth2-2h7m
 //create trasporter(client side)
@@ -14,8 +31,15 @@ let transporter = nodemailer.createTransport({
         clientId: process.env.OAUTH_CLIENTID,
         clientSecret: process.env.OAUTH_CLIENT_SECRET,
         refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+        accessToken: process.env.OAUTH_ACCESS_TOKEN,
     },
+    tls: {
+        rejectUnauthorized: false
+    },
+
 });
+console.log(process.env.OAUTH_CLIENTID);
+
 
 transporter.verify((err, success) => {
     err
@@ -26,22 +50,28 @@ transporter.verify((err, success) => {
 //create mailOption(serverside)
 
 let mailOptions = {
-    from: "mymail@gmail.com", // sender email
-    to: process.env.EMAIL, // receiver email
-    subject: "Nodemailer API",
-    text: "Hi from your nodemailer API",
+    from: "test@gmail.com", // sender email
+    to: "satyad18@gmail.com", // receiver email
+    subject: "Keep in Touch",
+    text: "Hello, Welcome to Fractal World !!",
 };
 
-//send the mailOptions through a transporter sendMail method
-transporter.sendMail(mailOptions, function (err, data) {
-    if (err) {
-        console.log("Error " + err);
-    } else {
-        console.log("Email sent successfully");
-    }
-});
 
 const port = 3001;
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-}); 
+});
+
+app.post('/contact', () => {
+    //send the mailOptions through a transporter sendMail method
+    console.log('post');
+    transporter.sendMail(mailOptions, function (err, data) {
+
+        console.log(mailOptions);
+        if (err) {
+            console.log("Error " + err.message);
+        } else {
+            console.log("Email sent successfully");
+        }
+    });
+})
